5428bb1077268afdda494d05a108c17221728420
Ben Merchant <benjaminmerchant@gmail.com>
2019-03-22 00:57:59 -0400

    just put it in prepack index.html

--- src/app/components/home/home.html.js
+++ src/app/components/home/home.html.js
@@ -1,9 +1,15 @@
+import navStyles from './home.scss';
 export const homeTemplate =
   `
   <div class='home-container flex-100-wide'>
     <h3>
-      home to my site!
+      Welcome to my site.
     </h3>
+  </div>
+  <div id="icon-arena">
+    <i class="fab fa-angular"></i>
+    <i class="fab fa-aws"></i>
+    <i class="fab fa-node-js"></i>
   </div>
   <div class='home-container flex-100-wide'>
     <h3>
