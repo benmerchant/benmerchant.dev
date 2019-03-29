52f286c6dc8a878025f408a9c2b4dc0962d81078
Ben Merchant <benjaminmerchant@gmail.com>
2019-03-29 01:38:19 -0400

    CORS issue just.... works.... idk why

--- src/app/components/home/home.service.js
+++ src/app/components/home/home.service.js
@@ -5,36 +5,22 @@ export class HomeService {
     // need the home route for home methods
     this.apiUrl = `${Globalvars.apiUrl}/home`;
     this.$http = $http;
-
-    // i spent so much time on this. Im not sending the statuses back right
-
-    // hard code apiURL real quick...
-    // get teh static Array from the back end
-    // next we'll do MongoDB
-
-    // lets split it up. still not wokring. Postman and Browswer API call are working
     this.method = 'GET';
-    this.url = 'localhost:3005/api/home';
+    this.url = 'http://localhost:3005/api/home';
+}
 
 
-
-    this.getStaticArray = (cb) => {
+    getStaticArray(cb) {
       this.$http({
         method: this.method,
         url: this.url
       }).then(
-
         function successCallback(response){
           if(response.status===200) {cb(response.data);}
         },
-
       function errorCallback(response) {
         if(errorCallback.status===400) {cb('error');}
-      })
+      })};
 
-
-
-    }};};
-
-
+};
 HomeService.$inject = ['$http','Globalvars'];
